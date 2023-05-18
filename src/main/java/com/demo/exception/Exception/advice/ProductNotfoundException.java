71fed1892b27b7d54da2d7e12547213ad05e74d9
Chhote Pal (HCL TECHNOLOGIES CORPORATE SER) <v-chhotepal@microsoft.com>
2023-05-18 12:49:54 +0530

    Spring boot Exception handle

---
+++ src/main/java/com/demo/exception/Exception/advice/ProductNotfoundException.java
@@ -0,0 +1,5 @@
+package com.demo.exception.Exception.advice;
+
+public class ProductNotfoundException  extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/test/java/com/demo/exception/Exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.exception.Exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
